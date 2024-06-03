9118705931544d4a0362b569aa4a77680ccd6370
defilippi82 <f.defilippi@gmail.com>
2024-06-03 15:09:11 -0300

    scss

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App.jsx'
 import './scss/index.css'
-import  "./scss/styles.scss"
+import  './scss/styles.scss'
+import "bootstrap/dist/css/bootstrap.min.css";
 import { UserProvider } from './components/UserContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
