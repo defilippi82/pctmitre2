3fafb371f39320f339eef1647ae072f99dd6dcae
Federico H. De Filippi <53954795+defilippi82@users.noreply.github.com>
2024-06-17 08:41:04 -0300

    foo

--- src/Views/Footer.jsx
+++ src/Views/Footer.jsx
@@ -14,7 +14,7 @@ export const Footer = () => {
            class="btn btn-outline-light btn-floating m-1"
            href="mailto:federico.filippi@trenesargentinos.gob.ar"
            role="button"
-           ><i class="fab fa-train"></i
+           ><i class="fa fa-train fa-8x"></i
           ></a>
 
         <a
