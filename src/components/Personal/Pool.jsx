89a2ee3f5ad4e445cdf16f7bfc0d93cc8d4e30d8
Federico H. De Filippi <53954795+defilippi82@users.noreply.github.com>
2024-06-20 14:21:21 -0300

    pool

--- src/components/Personal/Pool.jsx
+++ src/components/Personal/Pool.jsx
@@ -1,59 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { collection, query, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../../firebaseConfig/firebase';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Carousel from 'react-bootstrap/Carousel';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
+import CardGroup from 'react-bootstrap/CardGroup';
 import { CardFooter, CardHeader } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
 
-export const Pool = () => {
-    const [conductores, setConductores] = useState([]);
-    const [guardatrenes, setGuardatrenes] = useState([]);
-    const [rol, setRol] = useState('');
+export const Pool2 = () => {
+    const [currentView, setCurrentView] = useState('');
     const [base, setBase] = useState('');
-    const [currentView, setCurrentView] = useState('conductores');
-    const [currentPage, setCurrentPage] = useState(0);
+    const [rol, setRol] = useState('');
+    const [conductores, setConductores] = useState([]);
+    const [guardatren, setGuardatren] = useState([]);
     const [cardStates, setCardStates] = useState({});
+    const [currentPage, setCurrentPage] = useState(0);
 
-    const itemsPerPage = 4;
-    
-    //panel de busqueada
-    
-    // Firestore collections
     const conductoresCollection = collection(db, "conductores");
     const guardatrenesCollection = collection(db, "guardatren");
+    const itemsPerPage = 4;
+
     const handleBaseChange = (event) => setBase(event.target.value);
     const handleCurrentViewChange = (event) => setCurrentView(event.target.value);
     const handleRolChange = (event) => setRol(event.target.value);
-    const handleShow = () => {
-        if (currentView === 'conductores') {
-            fetchConductores();
-        } else if (currentView === 'guardatren') {
-            fetchGuardatren();
-        }
-    };
 
     useEffect(() => {
         if (currentView) {
             handleShow();
         }
     }, [currentView]);
-    
+
     const fetchConductores = async () => {
         try {
             let q = query(conductoresCollection);
-
-            if (base) {
-                q = query(q, where('base', '==', base));
-            }
-            if (rol) {
-                q = query(q, where('rol', '==', rol));
-            }
-
+            if (base) q = query(q, where('base', '==', base));
+            if (rol) q = query(q, where('rol', '==', rol));
             const querySnapshot = await getDocs(q);
             const data = querySnapshot.docs.map(doc => doc.data()).sort((a, b) => a.orden - b.orden);
             setConductores(data);
@@ -65,42 +50,22 @@ export const Pool = () => {
     const fetchGuardatren = async () => {
         try {
             let q = query(guardatrenesCollection);
-            if (base) {
-                q = query(q, where('base', '==', base));
-            }
+            if (base) q = query(q, where('base', '==', base));
             const querySnapshot = await getDocs(q);
             const data = querySnapshot.docs.map(doc => doc.data());
             setGuardatren(data);
         } catch (error) {
             console.error('Error fetching guardatren:', error);
         }
-    };// Número de elementos por página
-
-    /*const fetchConductores = async () => {
-        const q = query(collection(db, 'conductores'));
-        const querySnapshot = await getDocs(q);
-        const data = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-        setConductores(data);
     };
 
-    const fetchGuardatrenes = async () => {
-        const q = query(collection(db, 'guardatrenes'));
-        const querySnapshot = await getDocs(q);
-        const data = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-        setGuardatrenes(data);
+    const handleShow = () => {
+        if (currentView === 'conductores') {
+            fetchConductores();
+        } else if (currentView === 'guardatren') {
+            fetchGuardatren();
+        }
     };
-
-    useEffect(() => {
-        fetchConductores();
-        fetchGuardatrenes();
-    }, []);
-
-    const handleViewChange = (event) => {
-        setCurrentView(event.target.value);
-        setCardStates({}); // Limpiar el estado de las tarjetas al cambiar de vista
-    };*/
-
-    
 
     const handleAtendio = (id) => {
         setCardStates(prevState => ({
@@ -116,28 +81,18 @@ export const Pool = () => {
         }));
     };
 
-    const filteredData = (currentView === 'conductores' ? conductores : guardatrenes).filter(person => {
-        return (base ? person.base.toLowerCase().includes(base.toLowerCase()) : true);
-    });
-
-    // Ordenar por el campo 'orden'
+    const filteredData = currentView === 'conductores' ? conductores : guardatren;
     const sortedData = filteredData.sort((a, b) => a.orden - b.orden);
-
-    // Obtener el número total de páginas
     const totalPages = Math.ceil(sortedData.length / itemsPerPage);
-
-    // Dividir los datos en páginas
     const paginatedData = sortedData.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
 
-    const handleSelect = (selectedIndex, e) => {
+    const handleSelect = (selectedIndex) => {
         setCurrentPage(selectedIndex);
     };
 
     return (
         <Container>
-            <Row>
-                <Col>
-                <Form>
+            <Form>
                 <Form.Group controlId="baseSelect">
                     <Form.Label>Base Operativa</Form.Label>
                     <Form.Control as="select" value={base} onChange={handleBaseChange}>
@@ -171,42 +126,42 @@ export const Pool = () => {
                 )}
                 <Button variant="warning" onClick={handleShow}>Mostrar</Button>
             </Form>
-                    
-                </Col>
-            </Row>
             <Carousel activeIndex={currentPage} onSelect={handleSelect} interval={null}>
                 {Array.from({ length: totalPages }).map((_, pageIndex) => (
                     <Carousel.Item key={pageIndex}>
-                        <Row>
-                            {paginatedData
-                                .slice(pageIndex * itemsPerPage, (pageIndex + 1) * itemsPerPage)
-                                .map((person, index) => (
-                                    <Col key={index} xs={12} md={6} lg={3}>
+                        <CardGroup>
+                            <Row>
+                                {paginatedData.map((person, index) => (
+                                    <Col key={index} sm={12} md={6} lg={4}>
                                         <Card
-                                            style={{
-                                                backgroundColor: currentView === 'conductores' ? 'orange' : 'lightblue',
-                                                margin: '10px'
-                                            }}
+                                            bg={cardStates[person.legajo]?.color || 'light'}
+                                            text={(cardStates[person.legajo]?.color) ? 'dark' : 'black'}
+                                            style={{ width: '18rem', display: cardStates[person.legajo]?.hidden ? 'none' : 'block',  backgroundColor: currentView === 'conductores' ? 'orange' : 'lightblue',
+                                            margin: '10px' }}
+                                            className="mb-2"
                                         >
-                                            <Card.Header>{person.apellido}</Card.Header>
+                                            <CardHeader><strong>{person.nombre} {person.apellido}</strong></CardHeader>
                                             <Card.Body>
-                                                <p>Nombre: {person.nombre}</p>
-                                                <p>Rol: {person.rol}</p>
-                                                <p>Legajo: {person.legajo}</p>
-                                                <p>Teléfono: {person.telefono}</p>
-                                                <p>Orden: {person.orden}</p>
-                                                <p>Cupon: {person.cupon}</p>
-                                                <p>Apto: {person.apto}</p>
-                                                <p>Secciones: {person.secciones.join(', ')}</p>
+                                                <Card.Title>{currentView === 'conductores' ? `Cond.: ${person.rol}` : 'Guarda Tren'}</Card.Title>
+                                                <Card.Text><strong>Legajo:</strong> {person.legajo}</Card.Text>
+                                                <Card.Text><strong>Teléfono:</strong> {person.tel}</Card.Text>
+                                                <Card.Text><strong>Base:</strong> {person.base}</Card.Text>
+                                                <Card.Text><strong>Apto:</strong> {person.apto}</Card.Text>
+                                                <Card.Text><strong>Cupón:</strong> {person.cupon}</Card.Text>
+                                                <Card.Text><strong>Tarea:</strong> {person.tarea}</Card.Text>
+                                                <Card.Text><strong>Servicio:</strong> {person.servicio}</Card.Text>
+                                                <Card.Text><strong>Orden:</strong> {person.orden}</Card.Text>
+                                                {currentView === 'conductores' && <Card.Text><strong>Secciones:</strong> {person.secciones.join(', ')}</Card.Text>}
                                             </Card.Body>
                                             <CardFooter>
-                                                <Button variant="primary" onClick={() => handleAtendio(person.id)}>Atendió</Button>
-                                                <Button variant="danger" onClick={() => handleNoAtendio(person.id)}>No Atiende</Button>
+                                                <Button variant="primary" onClick={() => handleAtendio(person.legajo)}>Atendió</Button>
+                                                <Button variant="danger" onClick={() => handleNoAtendio(person.legajo)}>No Atiende</Button>
                                             </CardFooter>
                                         </Card>
                                     </Col>
                                 ))}
-                        </Row>
+                            </Row>
+                        </CardGroup>
                     </Carousel.Item>
                 ))}
             </Carousel>
--- src/components/Personal/Pool2.jsx
+++ src/components/Personal/Pool2.jsx
@@ -1,44 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import { collection, query, getDocs } from 'firebase/firestore';
 import { db } from '../../firebaseConfig/firebase';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Carousel from 'react-bootstrap/Carousel';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Form from "react-bootstrap/Form";
-import Button from 'react-bootstrap/Button';
-import CardGroup from 'react-bootstrap/CardGroup';
+import Form from 'react-bootstrap/Form';
 import { CardFooter, CardHeader } from 'react-bootstrap';
-import Carousel from 'react-bootstrap/Carousel';
 
-export const Pool2 = () => {
-    const [currentView, setCurrentView] = useState('');
-    const [base, setBase] = useState('');
-    const [rol, setRol] = useState('');
+export const Pool = () => {
     const [conductores, setConductores] = useState([]);
-    const [guardatren, setGuardatren] = useState([]);
-    const [cardStates, setCardStates] = useState({});
+    const [guardatrenes, setGuardatrenes] = useState([]);
+    const [rol, setRol] = useState('');
+    const [base, setBase] = useState('');
+    const [currentView, setCurrentView] = useState('conductores');
     const [currentPage, setCurrentPage] = useState(0);
+    const [cardStates, setCardStates] = useState({});
 
+    const itemsPerPage = 4;
+    
+    //panel de busqueada
+    
+    // Firestore collections
     const conductoresCollection = collection(db, "conductores");
     const guardatrenesCollection = collection(db, "guardatren");
-    const itemsPerPage = 4;
-
     const handleBaseChange = (event) => setBase(event.target.value);
     const handleCurrentViewChange = (event) => setCurrentView(event.target.value);
     const handleRolChange = (event) => setRol(event.target.value);
+    const handleShow = () => {
+        if (currentView === 'conductores') {
+            fetchConductores();
+        } else if (currentView === 'guardatren') {
+            fetchGuardatren();
+        }
+    };
 
     useEffect(() => {
         if (currentView) {
             handleShow();
         }
     }, [currentView]);
-
+    
     const fetchConductores = async () => {
         try {
             let q = query(conductoresCollection);
-            if (base) q = query(q, where('base', '==', base));
-            if (rol) q = query(q, where('rol', '==', rol));
+
+            if (base) {
+                q = query(q, where('base', '==', base));
+            }
+            if (rol) {
+                q = query(q, where('rol', '==', rol));
+            }
+
             const querySnapshot = await getDocs(q);
             const data = querySnapshot.docs.map(doc => doc.data()).sort((a, b) => a.orden - b.orden);
             setConductores(data);
@@ -50,22 +65,42 @@ export const Pool2 = () => {
     const fetchGuardatren = async () => {
         try {
             let q = query(guardatrenesCollection);
-            if (base) q = query(q, where('base', '==', base));
+            if (base) {
+                q = query(q, where('base', '==', base));
+            }
             const querySnapshot = await getDocs(q);
             const data = querySnapshot.docs.map(doc => doc.data());
             setGuardatren(data);
         } catch (error) {
             console.error('Error fetching guardatren:', error);
         }
+    };// Número de elementos por página
+
+    /*const fetchConductores = async () => {
+        const q = query(collection(db, 'conductores'));
+        const querySnapshot = await getDocs(q);
+        const data = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+        setConductores(data);
     };
 
-    const handleShow = () => {
-        if (currentView === 'conductores') {
-            fetchConductores();
-        } else if (currentView === 'guardatren') {
-            fetchGuardatren();
-        }
+    const fetchGuardatrenes = async () => {
+        const q = query(collection(db, 'guardatrenes'));
+        const querySnapshot = await getDocs(q);
+        const data = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+        setGuardatrenes(data);
     };
+
+    useEffect(() => {
+        fetchConductores();
+        fetchGuardatrenes();
+    }, []);
+
+    const handleViewChange = (event) => {
+        setCurrentView(event.target.value);
+        setCardStates({}); // Limpiar el estado de las tarjetas al cambiar de vista
+    };*/
+
+    
 
     const handleAtendio = (id) => {
         setCardStates(prevState => ({
@@ -81,18 +116,28 @@ export const Pool2 = () => {
         }));
     };
 
-    const filteredData = currentView === 'conductores' ? conductores : guardatren;
+    const filteredData = (currentView === 'conductores' ? conductores : guardatrenes).filter(person => {
+        return (base ? person.base.toLowerCase().includes(base.toLowerCase()) : true);
+    });
+
+    // Ordenar por el campo 'orden'
     const sortedData = filteredData.sort((a, b) => a.orden - b.orden);
+
+    // Obtener el número total de páginas
     const totalPages = Math.ceil(sortedData.length / itemsPerPage);
+
+    // Dividir los datos en páginas
     const paginatedData = sortedData.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
 
-    const handleSelect = (selectedIndex) => {
+    const handleSelect = (selectedIndex, e) => {
         setCurrentPage(selectedIndex);
     };
 
     return (
         <Container>
-            <Form>
+            <Row>
+                <Col>
+                <Form>
                 <Form.Group controlId="baseSelect">
                     <Form.Label>Base Operativa</Form.Label>
                     <Form.Control as="select" value={base} onChange={handleBaseChange}>
@@ -126,44 +171,46 @@ export const Pool2 = () => {
                 )}
                 <Button variant="warning" onClick={handleShow}>Mostrar</Button>
             </Form>
+                    
+                </Col>
+            </Row>
             <Carousel activeIndex={currentPage} onSelect={handleSelect} interval={null}>
                 {Array.from({ length: totalPages }).map((_, pageIndex) => (
                     <Carousel.Item key={pageIndex}>
-                        <CardGroup>
-                            <Row>
-                                {paginatedData.map((person, index) => (
-                                    <Col key={index} sm={12} md={6} lg={4}>
+                        <Row>
+                            {paginatedData
+                                .slice(pageIndex * itemsPerPage, (pageIndex + 1) * itemsPerPage)
+                                .map((person, index) => (
+                                    <Col key={index} xs={12} md={6} lg={3}>
                                         <Card
-                                            bg={cardStates[person.legajo]?.color || 'light'}
-                                            text={(cardStates[person.legajo]?.color) ? 'dark' : 'black'}
-                                            style={{ width: '18rem', display: cardStates[person.legajo]?.hidden ? 'none' : 'block' }}
-                                            className="mb-2"
+                                            style={{
+                                                backgroundColor: currentView === 'conductores' ? 'orange' : 'lightblue',
+                                                margin: '10px'
+                                            }}
                                         >
-                                            <CardHeader><strong>{person.nombre} {person.apellido}</strong></CardHeader>
+                                            <Card.Header>{person.apellido}</Card.Header>
                                             <Card.Body>
-                                                <Card.Title>{currentView === 'conductores' ? `Cond.: ${person.rol}` : 'Guarda Tren'}</Card.Title>
-                                                <Card.Text><strong>Legajo:</strong> {person.legajo}</Card.Text>
-                                                <Card.Text><strong>Teléfono:</strong> {person.tel}</Card.Text>
-                                                <Card.Text><strong>Base:</strong> {person.base}</Card.Text>
-                                                <Card.Text><strong>Apto:</strong> {person.apto}</Card.Text>
-                                                <Card.Text><strong>Cupón:</strong> {person.cupon}</Card.Text>
-                                                <Card.Text><strong>Tarea:</strong> {person.tarea}</Card.Text>
-                                                <Card.Text><strong>Servicio:</strong> {person.servicio}</Card.Text>
-                                                <Card.Text><strong>Orden:</strong> {person.orden}</Card.Text>
-                                                {currentView === 'conductores' && <Card.Text><strong>Secciones:</strong> {person.secciones.join(', ')}</Card.Text>}
+                                                <p>Nombre: {person.nombre}</p>
+                                                <p>Rol: {person.rol}</p>
+                                                <p>Legajo: {person.legajo}</p>
+                                                <p>Teléfono: {person.telefono}</p>
+                                                <p>Orden: {person.orden}</p>
+                                                <p>Cupon: {person.cupon}</p>
+                                                <p>Apto: {person.apto}</p>
+                                                <p>Secciones: {person.secciones.join(', ')}</p>
                                             </Card.Body>
                                             <CardFooter>
-                                                <Button variant="primary" onClick={() => handleAtendio(person.legajo)}>Atendió</Button>
-                                                <Button variant="danger" onClick={() => handleNoAtendio(person.legajo)}>No Atiende</Button>
+                                                <Button variant="primary" onClick={() => handleAtendio(person.id)}>Atendió</Button>
+                                                <Button variant="danger" onClick={() => handleNoAtendio(person.id)}>No Atiende</Button>
                                             </CardFooter>
                                         </Card>
                                     </Col>
                                 ))}
-                            </Row>
-                        </CardGroup>
+                        </Row>
                     </Carousel.Item>
                 ))}
             </Carousel>
         </Container>
     );
 };
+
